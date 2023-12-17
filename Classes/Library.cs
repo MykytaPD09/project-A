bdcd8c7a67275897e4d3c4edd61a7ef386b76c0e
Mykyta <nikita.ua1311@gmail.com>
2023-12-17 23:20:30 +0200

    first commit

--- Classes/Library.cs
+++ Classes/Library.cs
@@ -8,17 +8,3 @@
         public Librarian Librarian { get; set; }
     }
 }
-
-// Descr:
-// 1. Змінив Location на Address для більшої коректності.
-// 2. Змінив тип Librarian з Person на Librarian так як, Person у нас
-// немає в програмі, а встановлювати IPerson не має сенсу, адже тоді
-// виникає наступна ситуація.
-
-// Ситуація: -- Хто ти воїн? Я біблотекар --
-// (Тобто бібліотекарем нашої біблотеки може бути кожен хто реалізує IPerson)
-//
-// Приклад:
-// Library library;
-// library.Librarian = new Author(); - це не визве жодної помилки
-// адже Autor реалізує IPerson
